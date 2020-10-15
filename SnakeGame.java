42864c0df602042e1ee465c0b3a2ba9f7a5e2dcd
aneeqahmadh@gmail.com <37417454+shaneeqa@users.noreply.github.com>
2020-10-15 23:39:38 +0530

    Added token to the screen

--- SnakeGame.java
+++ SnakeGame.java
@@ -14,6 +14,7 @@ public class SnakeGame extends Applet implements Runnable, KeyListener {
     Thread thread;
     Snake snake;
     boolean gameOver;
+    Token token;
 
 
     public void init(){
@@ -23,6 +24,7 @@ public class SnakeGame extends Applet implements Runnable, KeyListener {
         gfx = img.getGraphics();
         this.addKeyListener(this);
         snake = new Snake();
+        token = new Token(snake);
         thread = new Thread(this);
         thread.start();
     }
@@ -34,6 +36,7 @@ public class SnakeGame extends Applet implements Runnable, KeyListener {
         gfx.fillRect(0, 0, 400, 400);
         if(!gameOver){
             snake.draw(gfx);
+            token.draw(gfx);
         } else{
             gfx.setColor(Color.RED);
             gfx.drawString("Game Over", 180, 150);
