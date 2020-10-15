eae22dc9442f137ee4c6eb93dc25491c044232e8
aneeqahmadh@gmail.com <37417454+shaneeqa@users.noreply.github.com>
2020-10-15 17:40:34 +0530

    Update till snake is on the screen

---
+++ Snake.java
@@ -0,0 +1,78 @@
+package Snake;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Snake {
+    /**
+     * snake gonna be represented by list of points to be modified
+     * snake runs over a token
+     */
+
+    List<Point> snakePoints; //front of the snake always gonna be element 0 of this list
+    int xDir, yDir;
+    boolean isMoving, elongate;
+    final int STARTSIZE = 20, STARTX = 150, STARTY = 150;
+
+    public Snake(){
+        snakePoints = new ArrayList<Point>();
+        xDir = 0;
+        yDir = 0; //xdir, ydir = 0 means snake is not moving
+        isMoving = false;
+        elongate = false;
+        snakePoints.add(new Point(STARTX, STARTY));
+        for(int i = 1; i < STARTSIZE; i++){
+            snakePoints.add(new Point(STARTX - i * 4, STARTY)); //push the snake body to the left
+        }
+    }
+
+    //create a snake object in snake game
+    public void draw(Graphics g){
+        g.setColor(Color.white);
+        //draw each points in the snake, snake is made up of several tiny rectangles
+        for (Point p : snakePoints){
+            g.fillRect(p.getX(), p.getY(), 4, 4);
+        }
+    }
+
+    public void move(){
+        if(isMoving){
+            /**
+             * Both temp and last defines where the snake currently is
+             */
+            Point temp = snakePoints.get(0);
+            Point last = snakePoints.get(snakePoints.size()-1); //
+            Point newStart = new Point(temp.getX() + xDir * 4, temp.getY() + yDir * 4);
+            for(int i = snakePoints.size() - 1; i >= 1; i--){
+                snakePoints.set(i, snakePoints.get(i - 1));
+            }
+            snakePoints.set(0, newStart);
+        }
+    }
+
+    public int getXDir(){
+        return xDir;
+    }
+
+    public int getYDir(){
+        return yDir;
+    }
+
+    public void setXDir(int xDir){
+        this.xDir = xDir;
+    }
+
+    public void setYDir(int yDir){
+        this.yDir = yDir;
+    }
+
+    //x position of the head of the snake
+    public int getX(){
+        return snakePoints.get(0).getX();
+    }
+
+    public int getY(){
+        return snakePoints.get(0).getY();
+    }
+}
---
+++ SnakeGame.java
@@ -0,0 +1,95 @@
+package Snake;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+//controls the size of the applet and the background
+
+public class SnakeGame extends Applet implements Runnable, KeyListener {
+
+    Graphics gfx;
+    Image img;
+    Thread thread;
+    Snake snake;
+
+
+    public void init(){
+        this.resize(400, 400);
+        img = createImage(400, 400);
+        gfx = img.getGraphics();
+        snake = new Snake();
+        thread = new Thread(this);
+        thread.start();
+    }
+
+
+    public void paint(Graphics g){
+        //off screen graphics
+        gfx.setColor(Color.black);
+        gfx.fillRect(0, 0, 400, 400);
+        snake.draw(gfx);
+        //image
+        g.drawImage(img, 0,0, null);
+    }
+
+    public void update(Graphics g){
+        paint(g);
+    }
+
+    public void repaint(Graphics g){
+        paint(g);
+    }
+
+    public void run() {
+        for(;;){
+
+            snake.move();
+
+            this.repaint();
+            try {
+                Thread.sleep(40);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_UP){
+            //if going down already can't go up/reverse the action
+            //if the snake is not going down we're allowing it to go up
+            if(snake.getYDir() != 1){
+                snake.setYDir(-1);
+                snake.setXDir(0);
+            }
+        }
+        if(e.getKeyCode() == KeyEvent.VK_DOWN){
+            if(snake.getYDir() != -1){
+                snake.setYDir(1);
+                snake.setXDir(0);
+            }
+        }
+        if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            if(snake.getXDir() != 1){
+                snake.setXDir(-1);
+                snake.setYDir(0);
+            }
+        }
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            if(snake.getXDir() != -1){
+                snake.setXDir(1);
+                snake.setYDir(0);
+            }
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
